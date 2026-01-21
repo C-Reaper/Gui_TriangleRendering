a35fa02d5f93f80af79442868eb02fbdee08cb78
codeleaded <gnu.c.reaper@gmail.com>
2026-01-21 17:31:22 +0100

    updated

--- src/Main.c
+++ src/Main.c
@@ -5,15 +5,22 @@
 Vec2* selected = NULL;
 TransformedView tv;
 Triangle triangle;
+Sprite gc;
 
 void Setup(AlxWindow* w){
 	triangle = Triangle_New(
-		(Vec2){ 0.1f,0.1f },
-		(Vec2){ 0.1f,0.9f },
-		(Vec2){ 0.9f,0.9f }
+		(Vec2){ 10.0f,10.0f },
+		(Vec2){ 10.0f,90.0f },
+		(Vec2){ 90.0f,90.0f }
 	);
 
-	tv = TransformedView_New((Vec2){ GetWidth(),GetHeight() });
+	tv = TransformedView_Make(
+		(Vec2){ GetWidth(),GetHeight() },
+		(Vec2){ 0.0f,0.0f },
+		(Vec2){ 0.01f,0.01f },
+		(float)GetWidth() / (float)GetHeight()
+	);
+	gc = Sprite_New(GetWidth(),GetHeight());
 }
 
 void Update(AlxWindow* w){
@@ -45,12 +52,14 @@ void Update(AlxWindow* w){
 
 	Clear(LIGHT_BLUE);
 
-	const Vec2 p1 = TransformedView_WorldScreenPos(&tv,triangle.p1);
-	const Vec2 p2 = TransformedView_WorldScreenPos(&tv,triangle.p2);
-	const Vec2 p3 = TransformedView_WorldScreenPos(&tv,triangle.p3);
+	//const Vec2 p1 = TransformedView_WorldScreenPos(&tv,triangle.p1);
+	//const Vec2 p2 = TransformedView_WorldScreenPos(&tv,triangle.p2);
+	//const Vec2 p3 = TransformedView_WorldScreenPos(&tv,triangle.p3);
 	
-	Triangle_RenderXWire(WINDOW_STD_ARGS,p1,p2,p3,BLACK,1.0f);
-	Triangle_RenderX(WINDOW_STD_ARGS,p1,p2,p3,GREEN);
+	Rect_RenderXX(gc.img,gc.w,gc.h,0.0f,0.0f,gc.w,gc.h,WHITE);
+
+	Triangle_RenderX(gc.img,gc.w,gc.h,triangle.p1,triangle.p2,triangle.p3,GREEN);
+	Triangle_RenderXWire(gc.img,gc.w,gc.h,triangle.p1,triangle.p2,triangle.p3,BLACK,1.0f);
 
 	for(int i = 0;i<3;i++){
 		Vec2* p = (Vec2*)(&triangle) + i;
@@ -58,6 +67,10 @@ void Update(AlxWindow* w){
 		
 		//Circle_RenderX(WINDOW_STD_ARGS,ps,rs,RED);
 	}
+
+	const Vec2 pos = TransformedView_WorldScreenPos(&tv,(Vec2){ 0.0f,0.0f });
+	const Vec2 len = TransformedView_WorldScreenLength(&tv,(Vec2){ gc.w,gc.h });
+	Sprite_RenderResize(WINDOW_STD_ARGS,&gc,pos.x,pos.y,len.x,len.y);
 }
 
 void Delete(AlxWindow* w){
